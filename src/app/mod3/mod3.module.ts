ad15df81ed9501b257a68ee0c0792e3da42aea68
AlfonsoRG13 <alfonsorochin05@gmail.com>
2021-10-31 13:47:29 -0700

    Correcciones en los 3 componentes, se hizo width 100% y se centraron

--- src/app/mod3/mod3.module.ts
+++ src/app/mod3/mod3.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LargeInputTextComponent } from './large-input-text/large-input-text.component';
 import { DropDownListComponent } from './drop-down-list/drop-down-list.component';
+import { SelectDropDownComponent } from './select-drop-down/select-drop-down.component'
 //Librerias de MUI
 import {MatInputModule} from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select'
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
     LargeInputTextComponent,
-    DropDownListComponent
+    DropDownListComponent,
+    SelectDropDownComponent
   ],
   imports: [
     CommonModule,
@@ -20,7 +22,8 @@ import {MatSelectModule} from '@angular/material/select'
   ],
   exports:[
     LargeInputTextComponent,
-    DropDownListComponent
+    DropDownListComponent,
+    SelectDropDownComponent
   ] //Exportamos los componenetes para poder utilizarlos en otros modulos
 })
 export class Mod3Module { }
