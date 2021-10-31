1a16369b6dffc3853e0b9f35b275a63bdf34ecf6
AlfonsoRG13 <alfonsorochin05@gmail.com>
2021-10-30 19:55:52 -0600

    largeInputText terminado??

--- src/app/mod3/mod3.module.ts
+++ src/app/mod3/mod3.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LargeInputTextComponent } from './large-input-text/large-input-text.component';
 
-
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     LargeInputTextComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule
   ],
   exports:[LargeInputTextComponent] //Exportamos los componenetes para poder utilizarlos
 })
