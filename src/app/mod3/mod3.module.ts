08cbe6b6b4870f2a45c2aec81a3aef60f38f2c49
AlfonsoRG13 <alfonsorochin05@gmail.com>
2021-10-31 12:44:49 -0700

    dropDowList Terminado

--- src/app/mod3/mod3.module.ts
+++ src/app/mod3/mod3.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LargeInputTextComponent } from './large-input-text/large-input-text.component';
-
+import { DropDownListComponent } from './drop-down-list/drop-down-list.component';
+//Librerias de MUI
 import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select'
 
 @NgModule({
   declarations: [
-    LargeInputTextComponent
+    LargeInputTextComponent,
+    DropDownListComponent
   ],
   imports: [
     CommonModule,
-    MatInputModule
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
-  exports:[LargeInputTextComponent] //Exportamos los componenetes para poder utilizarlos
+  exports:[
+    LargeInputTextComponent,
+    DropDownListComponent
+  ] //Exportamos los componenetes para poder utilizarlos en otros modulos
 })
 export class Mod3Module { }
