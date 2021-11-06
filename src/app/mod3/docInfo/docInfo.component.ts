2418030a35d7e366f16710dc2b70edeebbb08202
AlfonsoRG13 <alfonsorochin05@gmail.com>
2021-11-06 12:36:47 -0700

    Añadí parámetros al docInfo para su cardDoc

--- src/app/mod3/docInfo/docInfo.component.ts
+++ src/app/mod3/docInfo/docInfo.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit,Input } from '@angular/core';
 export class DocInfoComponent implements OnInit {
   @Input() desc:string = "";
   @Input() tit:string = "";
+  @Input() nomDoc:string = "";
   constructor() { }
 
   ngOnInit(): void {
