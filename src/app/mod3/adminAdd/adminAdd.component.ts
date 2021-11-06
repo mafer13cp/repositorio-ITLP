8a86d4ff80622acb324e621376c351b3117115e7
AlfonsoRG13 <alfonsorochin05@gmail.com>
2021-11-06 12:30:37 -0700

    Cambio en los componentes, se añadieron parametros para hacerlos reutilizables 100%

--- src/app/mod3/adminAdd/adminAdd.component.ts
+++ src/app/mod3/adminAdd/adminAdd.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'adminAdd',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adminAdd.component.css']
 })
 export class AdminAddComponent implements OnInit {
-
+  @Input() ops:string[]=[];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/mod3/docInfo/docInfo.component.ts
+++ src/app/mod3/docInfo/docInfo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'docInfo',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./docInfo.component.css']
 })
 export class DocInfoComponent implements OnInit {
-
+  @Input() desc:string = "";
+  @Input() tit:string = "";
   constructor() { }
 
   ngOnInit(): void {
