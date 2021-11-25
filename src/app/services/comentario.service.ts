0c2069ed349fee9a85a3df0399dbc345d2e6a059
Maria Fernanda <mafer13cp@gmail.com>
2021-11-25 16:18:15 -0700

    Servicio de comentarios agregado, servicio de usuarios modificado

---
+++ src/app/services/comentario.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, observable } from 'rxjs';
+import { HttpParams } from '@angular/common/http';
+import { Comentario } from '../interfaces/comentario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ComentarioService {
+
+  baseUrl: string = 'https://itlp-repo.herokuapp.com';
+
+  constructor(private http:HttpClient) { }
+//#region GET
+  public getComsCount():Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/comentarios/count`);
+  }
+  public getComs():Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/comentarios`);
+  }
+  public getComById(id: string):Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/comentarios/${id}`);
+  }
+//#endregion
+
+//#region POST
+  public postCom(com: Comentario):Observable<any> {
+    const headers = {'content-type': 'application/json'};
+    const body = JSON.stringify(com);
+    return this.http.post(`${this.baseUrl}/comentarios`,body, {'headers': headers, observe: 'response'});
+  }
+//#endregion
+
+//#region PUT 
+  public putCom(com: Comentario):Observable<any> {
+    const headers = {'content-type': 'application/json'};
+    const body = JSON.stringify(com);
+    return this.http.put(`${this.baseUrl}/comentarios/${com.id}`,body, {'headers': headers, observe: 'response'});
+  }
+//#endregion
+
+//#region DELETE 
+  public delCom(id: string):Observable<any> {
+    const headers = {'content-type': 'application/json'};
+    return this.http.delete(`${this.baseUrl}/comentarios/${id}`, {'headers': headers, observe: 'response'});
+  }
+//#endregion
+
+//#region GET PARAMS 
+public filterComByDoc(doc: string):Observable<any> {
+  const paramsHttp = new HttpParams().set('fk_documento', doc);
+  return this.http.get<any>(`${this.baseUrl}/comentarios`,{params: paramsHttp});
+}
+public filterComByUsuario(usuario: string):Observable<any> {
+  const paramsHttp = new HttpParams().set('fk_usuario', usuario);
+  return this.http.get<any>(`${this.baseUrl}/comentarios`,{params: paramsHttp});
+}
+public getCom(num: number):Observable<any> {
+  const paramsHttp = new HttpParams().set('limit', num);
+  return this.http.get<any>(`${this.baseUrl}/comentarios`,{params: paramsHttp});
+}
+//#endregion
+}
